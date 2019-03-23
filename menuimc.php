7d67010dc8b76e9a6e3568d70d4dc40970b49160
FernandoJHP <46433662+FernandoJHP@users.noreply.github.com>
2019-03-23 00:55:34 -0600

    menuimc

--- menuimc.php
+++ menuimc.php
@@ -6,10 +6,33 @@
 </head>
 <body>
 	<?php include('nav.php') ?>
-	<main role="main" class="container">
-		<div class="row">
+	<div class="container">
+		<div class="card border-primary">
+			<div class="card-header bg-transparent border-primary">
+				<div align="left">
+					INDICE DE MASA CORPORAL (IMC)
+				</div>
+				<div align="right">
+					<a class="btn btn-primary" href="">Agregar</a>
+				</div>
+			</div>
+			<div class="card-body text-dark">
+				<h5 class="card-title">Nivel 1</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			</div>
+			<div class="card-footer bg-transparent border-primary" align="center">
+				<nav aria-label="Page navigation example">
+					<ul class="pagination justify-content-center">
+						<li class="page-item"><a class="page-link" href="#">Anterior</a></li>
+						<li class="page-item"><a class="page-link" href="#">1</a></li>
+						<li class="page-item"><a class="page-link" href="#">2</a></li>
+						<li class="page-item"><a class="page-link" href="#">3</a></li>
+						<li class="page-item"><a class="page-link" href="#">Siguiente</a></li>
+					</ul>
+				</nav>
+			</div>
 		</div>
-	</main>
+	</div>
 	<!--Script-->
 	<script src="javascript/imc.js"></script>
 	<?php include('script.php') ?>
